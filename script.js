66e1ec71ac0b6bd848c440f918435771ced6c13c
Ribera Owen <310042931@cart.ads>
2018-03-19 15:28:48 -0700

    did a few more things

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ var count = 0;
 function countUp() {
     count += 1;
     if (count > 2) {
-        count == 2;
+        count = 2;
     }
 }
 
 function countDown() {
     count -= 1;
     if (count < 0) {
-        count == 0
+        count = 0
     }
 }
 
 function webCar() {
     if (count == 0) {
-        document.getElementById("web").setAttribute("href", "");
+        document.getElementById("web").getElementsByTagName("img").setAttribute("src", "images/website-img/climate.PNG");
         return 0;
     } else if (count == 1) {
-        document.getElementById("web").setAttribute("href", "");
+        document.getElementById("web").getElementsByTagName("img").setAttribute("src", "images/website-img/ethics.PNG");
         return 1;
     } else if (count == 2) {
-        document.getElementById("web").setAttribute("href", "");
+        document.getElementById("web").getElementsByTagName("img").setAttribute("src", "images/website-img/zen.PNG");
         return 2;
     }
 }
